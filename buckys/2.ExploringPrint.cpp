8ed175700033d3e7d26fe40b57ed4576e38b9623
JeanC4rlo <jeancs.santos1@gmail.com>
2026-03-14 11:56:25 -0300

    Estudos relativos a https://youtu.be/sPv0HQ8xOaU?si=m7scTp5jDsqc8SC0

---
+++ buckys/2.ExploringPrint.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Sem quebra
+    cout << "Eu farmo muita aura, ";
+    cout << "sou aura mais ego";
+
+    cout << endl << endl;
+
+    // Com quebra (\n)
+    cout << "Eu farmo muita aura\n";
+    cout << "sou aura mais ego";
+    
+    cout << endl << endl;
+
+    // Com quebra (endl)
+    cout << "Eu farmo muita aura" << endl;
+    cout << "sou aura mais ego";
+
+    cout << endl;
+    return 0;
+}
